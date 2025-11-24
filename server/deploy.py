f3302c1b28462d0178773dfae4cb78135be128d8
Riley Poole <riley@example.com>
2025-11-23 19:29:53 -0800

    update

--- server/deploy.py
+++ server/deploy.py
@@ -1,7 +1,15 @@
 from util import Console
 import subprocess
+import random
+import json
 
-subprocess.run("clear")
 print("Starting server deploy script...")
 Console.run(["lsblk"])
+parts = input("What partitions would you like to deploy? [sda, sdb, ...]").split(" ")
 
+keys = []
+for part in parts:
+    keys.append((part,random.randbytes(128)))
+    
+with open("luks.out","w") as file:
+    file.write(json.dumps(keys))
