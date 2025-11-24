00a2dd68390ceb4241fc8bfaba0be0ddb6d58f15
Riley Poole <riley@example.com>
2025-11-23 18:11:00 -0800

    change deploy server

--- DeployServer/server.py
+++ DeployServer/server.py
@@ -3,8 +3,17 @@ import subprocess
 class Server:
 
     @staticmethod
+    def run(cmd, capture_output = True, check = True):
+        try:
+            subprocess.run(cmd,capture_output,check)
+        except subprocess.CalledProcessError as e:
+            print(e.stderr)
+
+
+    @staticmethod
     def install_depend():
-        subprocess.run(["apt","update"], check=True)
-        subprocess.run(["apt","install","python3-pip"],check=True)
-        subprocess.run(["pip3","install","PyYaml"], check=True)
+        Server.run(["apt","update","-y"])
+        Server.run(["apt","upgrade","-y"])
+        Server.run(["apt","install","python3-pip"])
+        Server.run(["pip3","install","PyYaml"])
 
