1c6d18e7290120faa2c9861b1d71d146e16967fc
Riley Poole <riley@example.com>
2025-11-23 16:02:24 -0800

    change deploy server

--- deploy-server.py
+++ deploy-server.py
@@ -5,5 +5,5 @@ import subprocess
 cur_part = subprocess.run(["lsblk"], capture_output=True, text=True)
 print(cur_part.stdout)
 
-use_part = input("Which partitions would you like to use?")
-print(use_part)
+use_part = input("Which partitions would you like to use? ")
+print(use_part.split(" "))
